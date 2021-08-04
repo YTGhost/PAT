3ff4e2e0f245f3be5bcb30839c55173645d52867
YTGhost <283304489@qq.com>
2021-08-05 00:39:08 +0800

    add 1047 modify 1140 2021/08/04

--- Advanced-Level-Practice/1140 Look-and-say Sequence.cpp
+++ Advanced-Level-Practice/1140 Look-and-say Sequence.cpp
@@ -3,6 +3,36 @@ using namespace std;
 
 int main()
 {
+    int d, n;
+    cin >> d >> n;
+    string str = "" + to_string(d);
+    for(int i = 1; i < n; i++) {
+        string t = "";
+        char pre = str[0];
+        int cnt = 1;
+        for(int j = 1; j < str.size(); j++) {
+            if(str[j] == pre) {
+                cnt++;
+            } else {
+                t += pre + to_string(cnt);
+                pre = str[j];
+                cnt = 1;
+            }
+        }
+        if(cnt) {
+            t += pre + to_string(cnt);
+        }
+        str = t;
+    }
+    printf("%s\n", str.c_str());
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
     string d;
     int n;
     cin >> d >> n;
